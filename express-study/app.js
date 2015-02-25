6d7fc702a665470ff7fd163d5dacdb4663f1a2b1
Sajin Saleem <saleemsajin@gmail.com>
2015-02-25 23:43:00 +0530

    Add feedback

--- express-study/app.js
+++ express-study/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var feedback = require('./routes/feedback');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/feedback', feedback);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- express-study/routes/feedback.js
+++ express-study/routes/feedback.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('feedback', { title: 'Feed back form' });
 });
 
 module.exports = router;
