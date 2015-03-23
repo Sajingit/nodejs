907e13a8c2c520366fc069f5a71bdecfb27b2d2f
Sajin Saleem <saleemsajin@gmail.com>
2015-03-23 22:43:58 +0530

    Add callback success in save function

--- express-study/model/register.js
+++ express-study/model/register.js
@@ -17,7 +17,67 @@ var UserDetailsSchema = mongoose.Schema({
 var UserDetails = mongoose.model('UserDetails', UserDetailsSchema, 'userdetails');  
 
 // make this 'UserDetails' model available to Node applications
-exports.UserDetails = UserDetails; 
+//exports.UserDetails = UserDetails; 
+
+
+
+/**
+ * Save user datas to mongodb
+ */
+exports.saveUserDatas = function(data, callback){
+	
+	//var email = require('../shared/email');
+	
+	UserDetails.count({ username: data.username}, function(err, count){
+		
+		if (err) throw err;
+
+		if(count > 0){
+			callback('Username is already in use');
+			
+		}else {
+			UserDetails.count({ email: data.email}, function(err, count){
+				if (err) throw err;
+				
+				if(count > 0){
+					callback('Email is already in use');
+				}else{
+					
+					var userDetail = UserDetails({
+						username: data.username,
+						password: data.password,
+						fname: data.fname,
+						lname: data.lname,
+						email: data.email,
+						comments: data.comments
+					});
+					
+					// call the built-in save method to save to the database
+					userDetail.save(function(err) { 
+					  if (err) throw err;
+					  //email.registerEmail(data);
+					  callback('SUCCESS');
+					});
+				}
+			});
+		}
+		
+		//var obj = initialFormObjects();console.log('Name::');console.log(name);console.log(duplicateMsg);
+		
+		//if(duplicateMsg){
+			
+			//var err = [{
+				//msg: duplicateMsg	
+			//}];
+			
+			//obj = data;
+			
+		//}
+		
+		//res.render('register', { title: 'Registration form', errors:err, data:obj});
+		
+	});
+}
 
 
 
--- express-study/routes/register.js
+++ express-study/routes/register.js
@@ -1,14 +1,13 @@
 //grab the things we need
 var express = require('express');
 var router  = express.Router();
-
+var email 	= require('../shared/email');
+var reg 	= require('../model/register');
 
 router.get('/', function(req, res) {
 	
-
 	var obj = initialFormObjects();
 	res.render('register', { title: 'Registration form', data:obj  });
-	
 	
 });
 
@@ -17,15 +16,6 @@ router.get('/', function(req, res) {
  * Handle post section in registration form
  */
 router.post('/', function(req, res) {
-	
-	//var user = require('../model/register');
-	
-	//user.UserCredentials.find([{ username: req.body.username }, { email: req.body.email }], function(err, count){
-		//console.log(count);
-	//});
-	
-	//return false;
-	
 	
 	//Form validation
 	req.assert('username', 'Usename field is empty').notEmpty();
@@ -47,9 +37,25 @@ router.post('/', function(req, res) {
 	}
 	else {
 		
-		saveUserDatas(req.body, res);
-		
-		
+		//Save user data to mongo userdata table
+		reg.saveUserDatas(req.body, function(msg){
+			if(msg != 'SUCCESS'){
+
+				var err = [{
+					msg: msg	
+				}];
+				
+				obj = req.body;
+				
+			}else{
+				console.log("a");
+				var obj = initialFormObjects();
+				email.registerEmail(req.body);
+			}
+			
+			res.render('register', { title: 'Registration form', errors:err, data:obj});
+			
+		});
 	}
 });
 
@@ -71,103 +77,6 @@ var initialFormObjects = function(){
 	return obj;
 	
 }
-
-
-/**
- * Save user datas to mongodb
- */
-var saveUserDatas = function(data, res){
-	
-	var user  = require('../model/register');
-	var email = require('../shared/email');
-	var err	  = {};
-	var duplicateMsg = null;
-	
-	user.UserDetails.count({ username: data.username}, function(err, count){
-		
-		if (err) throw err;
-
-		if(count > 0){
-			console.log("1");
-			var duplicateMsg = 'Username is already in use';
-			
-		}else {
-			console.log("2");
-			user.UserDetails.count({ email: data.email}, function(err, count){
-				if (err) throw err;
-				console.log("COUNT::");console.log(count);
-				if(count > 0){
-					console.log("3");
-					var duplicateMsg = 'Email is already in use';
-					var name = 'sajin';
-					
-				}else{
-					
-					console.log("4");	  
-					var userDetail = user.UserDetails({
-						username: data.username,
-						password: data.password,
-						fname: data.fname,
-						lname: data.lname,
-						email: data.email,
-						comments: data.comments
-					});
-					
-					// call the built-in save method to save to the database
-					userDetail.save(function(err) { console.log("5");
-					  if (err) throw err;
-	
-					  console.log('User Details saved successfully!');
-					  email.registerEmail(data);
-					});
-				}
-			});
-		}
-		
-		var obj = initialFormObjects();console.log('Name::');console.log(name);console.log(duplicateMsg);
-		
-		if(duplicateMsg){
-			
-			var err = [{
-				msg: duplicateMsg	
-			}];
-			
-			obj = data;
-			
-		}
-		
-		res.render('register', { title: 'Registration form', errors:err, data:obj});
-		
-	});
-}
-
-/**
- * Function used to get username is already in use
- * @username String - Username
- */
-var getUsernameCount = function(username, callback){
-	
-	var user = require('../model/register');
-	user.UserDetails.count({ username: username}, callback);
-
-}
-
-/**
- * Function used to get user email is already in use
- * @email String - Email
- */
-var getEmailCount = function(email){
-	
-	var user = require('../model/register');
-	
-	user.UserDetails.count({ email: email}, function (err, result){
-		return result;
-	});
-	
-}
-
-
-
 
 exports.initialFormObjects = initialFormObjects;
 module.exports = router;
